c5a4740c501d900f3a7de363c139e85a6b0c2726
Tima <lokitim94@mail.ru>
2017-04-18 16:47:49 +0000

    Update pac.cpp

--- pac.cpp
+++ pac.cpp
@@ -6,9 +6,10 @@
 #include <vector>
  
  
+const int Bit=100;
 const int DepWidth=1101;
 const int PacCount=752;
-const int MaxWidth=29;
+const int MaxWidth=30;
 int bitmap[10001][10001];
  
 using namespace std;
@@ -20,40 +21,104 @@ struct Package
     int depAmount;
     int depNumber[DepWidth];
     int level;
-    int priority;
     int xpos;
     int ypos;
 };
  
  
-void CountLevels(Package p[PacCount], int num)
+void CountLevels(Package p[PacCount+1], int num)
 {
-	bool flag=true;
-    for ( int i = 1; i < p[num].depAmount; i++ )
+    bool flag=true;
+    for ( int i = 1; i < p[num].depAmount+1; i++ )
     {
-		for ( int j = 1; j < p[p[num].depNumber[i]].depAmount; j++ )
-		{
-			if(p[p[num].depNumber[i]].depNumber[j]==num)
-			{flag=false;}
-		}
-		if(flag==false)
-		{
-			if(p[p[num].depNumber[i]].level>p[num].level)
-			{p[p[num].depNumber[i]].level=p[num].level;}
-			else
-			{p[num].level=p[p[num].depNumber[i]].level;}
-		}
-		if ((p[p[num].depNumber[i]].level<=p[num].level)&&(flag==true))
-		{
-			p[p[num].depNumber[i]].level=p[num].level+1;
-			CountLevels(p,p[num].depNumber[i]);
-		}
+        for ( int j = 1; j < p[p[num].depNumber[i]].depAmount+1; j++ )
+        {
+            if(p[p[num].depNumber[i]].depNumber[j]==num)
+            {flag=false;}
+        }
+        if(flag==false)
+        {
+            if(p[p[num].depNumber[i]].level>p[num].level)
+            {p[p[num].depNumber[i]].level=p[num].level;}
+            else
+            {p[num].level=p[p[num].depNumber[i]].level;}
+        }
+        if ((p[p[num].depNumber[i]].level<=p[num].level)&&(flag==true))
+        {
+            p[p[num].depNumber[i]].level=p[num].level+1;
+            CountLevels(p,p[num].depNumber[i]);
+        }
     }
 }
  
+ 
+void DrawLine( int x1, int y1, int x2, int y2)
+{
+    const int deltaX = abs(x2 - x1);
+    const int deltaY = abs(y2 - y1);
+    const int signX = x1 < x2 ? 1 : -1;
+    const int signY = y1 < y2 ? 1 : -1;
+    //
+    int error = deltaX - deltaY;
+    //
+    bitmap[x2][y2]=0;
+    while(x1 != x2 || y1 != y2)
+   {
+        bitmap[x1][y1]=0;
+        const int error2 = error * 2;
+        //
+        if(error2 > -deltaY)
+        {
+            error -= deltaY;
+            x1 += signX;
+        }
+        if(error2 < deltaX)
+        {
+            error += deltaX;
+            y1 += signY;
+        }
+    }
+}
+ 
+ 
+void SearchDep(Package p[PacCount+1])
+{
+    for ( int i = 1; i < PacCount+1; i++ )
+    {
+        p[i].depAmount=0;
+    }
+    for ( int i = 1; i < PacCount+1; i++ )
+    {
+        string itchar="";  
+        int ij=strlen(p[i].dep);
+        for ( int j = 0; j < ij+1; j++ )
+        {                  
+            if ((p[i].dep[j]==' ') || (j==ij))
+            {              
+                for ( int z = 1; z < PacCount+1; z++ )
+                {
+                    if (itchar==""){break;}
+                    if (itchar==p[z].name)
+                    {
+                        p[i].depAmount=p[i].depAmount+1;
+                        p[i].depNumber[p[i].depAmount]=z;
+                        break;                                        
+                    }
+                }
+                itchar="";
+            }
+            else
+            {
+                itchar=itchar+p[i].dep[j];
+            }
+        }
+    }  
+}  
+   
+   
 int main()
 {
-    struct Package pac[PacCount],pac2;
+    struct Package pac[PacCount+1],pac2;
     string istring,itchar;
     int i;
    
@@ -63,20 +128,18 @@ int main()
  
  
     ifstream File("/home/lubuntu/code/var/Packages.dat");
-    i=0;
+    i=1;
     while(getline(File, istring))
     {
         strcpy( pac[i].name, istring.c_str() );
         pac[i].level=1;
-        pac[i].priority=0;
-        pac[i].depAmount=0;
         i++;
     }        
     File.close();
  
  
     ifstream file("/home/lubuntu/code/var/Required.dat");
-    i=0;
+    i=1;
     while(getline(file, istring))
     {
         strcpy( pac[i].dep, istring.c_str() );
@@ -86,86 +149,58 @@ int main()
  
  
 //-------------------------------------------------------------------------
-// Search deps
- 
- 
-    for ( int i = 0; i < PacCount; i++ )
-    {
-        string itchar="";  
-        int ij=strlen(pac[i].dep);
-        for ( int j = 0; j < ij+1; j++ )
-        {                  
-            if ((pac[i].dep[j]==' ') || (j==ij))
-            {              
-                for ( int z = 0; z < PacCount; z++ )
-                {
-                    if (itchar==""){break;}
-                    if (itchar==pac[z].name)
-                    {
-                        pac[i].depAmount=pac[i].depAmount+1;
-                        pac[i].depNumber[pac[i].depAmount]=z;
-                        break;                                         
-                    }
-                }
-                itchar=""; 
-            }
-            else
-            {
-                itchar=itchar+pac[i].dep[j];
-            }
-        }
-    }
- 
- 
+SearchDep(pac);
 //-------------------------------------------------------------------------
 // Count levels
  
  
-    for ( int i = 0; i < PacCount; i++ )
+    for ( int i = 1; i < PacCount+1; i++ )
     {
-		CountLevels(pac, i);
+        CountLevels(pac, i);
     }
  
  
-//-------------------------------------------------------------------------	
+//-------------------------------------------------------------------------
 // Sort Packages by level
-
-
-for ( int i = 0; i < PacCount; i++ )
-for ( int j = i; j < PacCount; j++ )
-{
-		if (pac[i].level>pac[j].level)
-		{
-			pac2=pac[i];
-			pac[i]=pac[j];
-			pac[j]=pac2;
-		}
-}
-
-
-//-------------------------------------------------------------------------	
+ 
+ 
+    for ( int i = 1; i < PacCount+1; i++ )
+    for ( int j = i; j < PacCount+1; j++ )
+    {
+        if (pac[i].level>pac[j].level)
+        {
+            pac2=pac[i];
+            pac[i]=pac[j];
+            pac[j]=pac2;
+        }
+    }
+   
+   
+//-------------------------------------------------------------------------
+SearchDep(pac);
+//-------------------------------------------------------------------------
 // Sort Packages by priority
+ 
 
-
-for ( int i = 0; i < PacCount-40; i=i+40 )
-for ( int j = 0; j < 39; j++ )
+for ( int i = 1; i < PacCount+1-40; i=i+40 )
+for ( int j = 1; j < 40; j++ )
 {
-	for ( int z = j; z < 39; z++ )
-	{
-		if (pac[i+j].depAmount<pac[i+z].depAmount)
-		{
-			pac2=pac[i+j];
-			pac[i+j]=pac[i+z];
-			pac[i+z]=pac2;
-		}
-	}
+    for ( int z = j+1; z < 40; z++ )
+    {
+        if (pac[i+j].depAmount<pac[i+z].depAmount)
+        {
+            pac2=pac[i+j];
+            pac[i+j]=pac[i+z];
+            pac[i+z]=pac2;
+        }
+    }
 }
 
-
-//-------------------------------------------------------------------------	
+ 
+//-------------------------------------------------------------------------
 // Create bitmap v
-
-
+ 
+ 
 int xi=0;
 int yi=0;
 //int ml=sqrt(PacCount)+1;
@@ -173,256 +208,136 @@ int yi=0;
 //int bmy=(ml+pac[0].level)*300;
 //int xbitmap[][9000];
 //xbitmap[bmx][bmy]=10;
-
-// Set top-left squad positions
-
-for ( int i = 0; i < PacCount; i++ )
+ 
+ 
+for ( int i = 1; i < PacCount+1; i++ )
 {
-	if(xi>=40)
-	{
-		xi=0;
-		yi=yi+1;
-	}
-	pac[i].xpos=xi*200;
-	pac[i].ypos=yi*300;
+    if(xi>=40)
+    {
+        xi=0;
+        yi=yi+1;
+    }
+    pac[i].xpos=xi*2*Bit;
+    pac[i].ypos=yi*3*Bit;
+    xi++;
+}
+ 
+	
+xi=0;
+yi=0;
+
+	
+for ( int i = 1; i < PacCount+1; i=i+2 )
+{
 	xi++;
+    pac[i].xpos=(44-xi*2)*Bit;
+    pac[i].ypos=yi*3*Bit+xi*20;
+    pac[i+1].xpos=(44+xi*2)*Bit;
+    pac[i+1].ypos=yi*3*Bit+xi*20;
+    if(xi>=20)
+    {
+        xi=0;
+        yi=yi+1;
+    }
 }
 
 
-for ( int i = 0; i < PacCount; i=i+2 )
-{
-	if(xi>=20)
-	{
-		xi=0;
-		yi=yi+1;
-	}
-	pac[i].xpos=4800-xi*200;
-	pac[i].ypos=4800+yi*300;
-	pac[i+1].xpos=5000+xi*200;
-	pac[i+1].ypos=4800+yi*300;
-	xi++;
-}
-
-
-//-------------------------------------------------------------------------	
+//-------------------------------------------------------------------------
 // zeroing bitmap v
-
-
+ 
+ 
 for ( int i = 0; i < 10000; i++ )
 for ( int j = 0; j < 10000; j++ )
 {
-	bitmap[i][j]=1;
+    bitmap[i][j]=1;
 }
-
-
-//-------------------------------------------------------------------------	
-// Write bitmap squad v
-
-
-for ( int i = 0; i < PacCount; i++ )
-{
-	for ( int x = 0; x < 140; x++ )
-	for ( int y = 0; y < 20; y++ )
-	{
-		bitmap[pac[i].xpos+30+x][pac[i].ypos+40+y]=0;
-	}
-}
-
-for ( int i = 0; i < PacCount; i++ )
-{
-	for ( int x = 1; x < 139; x++ )
-	for ( int y = 1; y < 19; y++ )
-	{
-		bitmap[pac[i].xpos+30+x][pac[i].ypos+40+y]=1;
-	}
-}
-
-
-//-------------------------------------------------------------------------	
-// Write bitmap depends
-
-
-for ( int i = 0; i < PacCount; i++ )
-{
-	string itchar="";	
-		int ij=strlen(pac[i].dep);
-		for ( int j = 0; j < ij+1; j++ )
-		{						
-			if(pac[i].dep[j]=='N')
-			{break;}			
-			if ((pac[i].dep[j]==' ') || (j==ij))
-			{					
-				for ( int z = 0; z < PacCount; z++ )
-				{
-					if (itchar==""){break;}
-					if (itchar==pac[z].name) 
-					{						
-						//write line from pac[i].xpos+170 pac[i].ypos+40 to pac[z].xpos+30 pac[z].ypos+60
-						// Bresenham's line algorithm
-						int x1=pac[i].xpos+170;
-						int y1=pac[i].ypos+60;
-						int x2=pac[z].xpos+30;
-						int y2=pac[z].ypos+40;
-						int dx=abs(x2-x1);
-						int dy=abs(y2-y1);
-						int fx=1;
-						if (x1>x2)
-						fx=-1;
-						int fy=1;
-						if (y1>y2)
-						fy=-1;
-						int e=0;
-						if(abs(x2-x1)>abs(y2-y1))
-						{
-							int de=dy;
-							int y=y1;
-							for (int x = x1; x < x2; x=x+fx)
-							{	
-								bitmap[x][y]=0;
-								e=e+de;
-								if(2*e>=dx)
-								{
-									y=y+fy;
-									e=e-dx;
-								}
-							}
-						}
-						else
-						{
-							int de=dx;
-							int x=x1;
-							for (int y = y1; y < y2; y=y+fy)
-							{	
-								bitmap[x][y]=0;
-								e=e+de;
-								if(2*e>=dy)
-								{
-									x=x+fx;
-									e=e-dy;
-								}
-							}
-						}
-						
-								
-						if (pac[i].level>=pac[z].level) 
-						{
-//							pac[z].level=pac[i].level+1;
-//							wl=1;
-//							cout << f << "\n";
-						}
-						break;						
-					}
-				}
-				itchar="";	
-			}
-			else
-			{
-				itchar=itchar+pac[i].dep[j];
-			}
-		}
-}
-	
-/*	
-						int x1=100;
-						int y1=100;
-						int x2=200;
-						int y2=400;
-						int dx=abs(x2-x1);
-						int dy=abs(y2-y1);
-						int fx=1;
-						if (x1>x2)
-						fx=-1;
-						int fy=1;
-						if (y1>y2)
-						fy=-1;
-						int e=0;
-						if((x2-x1)>(y2-y1))
-						{
-							int de=dy;
-							int y=y1;
-							for (int x = x1; x < x2; x=x+fx)
-							{	
-								bitmap[x][y]=0;
-								e=e+de;
-								if(2*e>=dx)
-								{
-									y=y+fy;
-									e=e-dx;
-								}
-							}
-						}
-						else
-						{
-							int de=dx;
-							int x=x1;
-							for (int y = y1; y < y2; y=y+fy)
-							{	
-								bitmap[x][y]=0;
-								e=e+de;
-								if(2*e>=dy)
-								{
-									x=x+fx;
-									e=e-dy;
-								}
-							}
-						}
-	*/	
-
-//-------------------------------------------------------------------------	
-// Write bitmap
-
-
-ofstream ofs("/home/lubuntu/code/var/Bitmaple.dat");
-for ( int j = 0; j < 10000; j++ )
-{
-	for ( int i = 0; i < 10000; i++ )
-	{
-		ofs << bitmap[i][j] << ' ';	
-	}	
-			ofs << "\n";	
-}
-ofs.close();
  
-
-//bitmap[1][100]=1;
-//cout << bitmap[1][100] << ' ' << bmx << ' ' << bmy << ' '<< endl;
-
-
-for ( int i = 0; i < PacCount; i++ )
+ 
+//-------------------------------------------------------------------------
+// Write bitmap squad v
+ 
+ 
+for ( int i = 1; i < PacCount+1; i++ )
 {
-//	cout << pac[i].name << ',' << pac[i].level << ',' << pac[i].priority << ':' << pac[i].xpos << ',' << pac[i].ypos <<',' << i << "\n";
-}	
-
-
+    for ( int x = 0; x < 140; x++ )
+    for ( int y = 0; y < 20; y++ )
+    {
+        bitmap[pac[i].xpos+30+x][pac[i].ypos+40+y]=0;
+    }
+}
+ 
+for ( int i = 1; i < PacCount+1; i++ )
+{
+    for ( int x = 1; x < 139; x++ )
+    for ( int y = 1; y < 19; y++ )
+    {
+        bitmap[pac[i].xpos+30+x][pac[i].ypos+40+y]=1;
+    }
+}
+ 
+ 
+//-------------------------------------------------------------------------
+// Write line from pac to dep
+ 
+ 
+for ( int i = 1; i < PacCount+1; i++ )
+{
+    for ( int j = 1; j < pac[i].depAmount+1; j++ )
+    {
+        cout << i << ';' << pac[i].depNumber[j] << ' ';
+        int x1=pac[i].xpos+170;
+        int y1=pac[i].ypos+60;
+        int x2=pac[pac[i].depNumber[j]].xpos+30;
+        int y2=pac[pac[i].depNumber[j]].ypos+40;
+        DrawLine(x2,y2,x1,y1);
+    }  
+    cout << endl;
+}
+ 
+ 
 //-------------------------------------------------------------------------
 // Test result
  
  
-for ( int i = 0; i < PacCount; i++ )
+ for ( int i = 1; i < PacCount+1; i++ )
+{
+    cout << pac[i].name << ',' << pac[i].level << ',' << pac[i].xpos <<',' << pac[i].ypos << ':';
+    cout << "\n";
+}
+ 
+ 
+ /*
+for ( int i = 1; i < PacCount+1; i++ )
 {
     cout << pac[i].name << ',' << pac[i].level << ':';
     for ( int j = 1; j < pac[i].depAmount+1; j++ )
     {
-//        cout << pac[i].depNumber[j] << ' ';
+        cout << pac[i].depNumber[j] << ' ';
     }
     cout << "\n";
 }
  
  
+//  DrawLine( x1, y1, x2, y2);
+    DrawLine(200,400,100,100);
+    DrawLine(100,500,100,100);     
+    DrawLine(500,200,100,200);
+    */
+	
+	
 //-------------------------------------------------------------------------
-// Tasks
+// Write bitmap
  
  
-cout << "cgroup разобраться как ограничить колличество выделяемой памяти процессу"<<endl;
-cout << "запуск приложения из субгруппы"<<endl; 
-cout << "приоритет v"<<endl;
-cout << "сортировка по приоритету(колличество зависимостей) v"<<endl;
-cout << "убрать отрисовку по уровням только по порядку v"<<endl;
-cout << "отрисовка из центра по 2 пакета влево вправо v"<<endl;
-cout << "съехала матрица вверх, нужно понять почему v"<<endl;
-cout << "переписать отрисовку зависимостей"<<endl;
-cout << "дерево не похоже на дерево -_____-"<<endl;	
-
-
-return 0;
+ofstream ofs("/home/lubuntu/code/var/Bitmaple.dat");
+for ( int j = 0; j < 10000; j++ )
+{
+    for ( int i = 0; i < 10000; i++ )
+    {
+        ofs << bitmap[i][j] << ' ';
+    }  
+            ofs << "\n";   
+}
+ofs.close();
+ 
 }
